f1cbb8a16b9ff63c6661f0757a05eb075609d44e
boeschf <48126478+boeschf@users.noreply.github.com>
2026-05-05 19:08:49 +0200

    executor

--- src/core/include/communicator.hpp
+++ src/core/include/communicator.hpp
@@ -80,8 +80,6 @@ struct Communicator::Impl {
   std::shared_ptr<BaseRecvItem> getLastRecvItem(int remoteRank, int tag);
 
   struct Connector;
-
-  //int peerRank(Connection const& connection) const;
 };
 
 }  // namespace mscclpp
